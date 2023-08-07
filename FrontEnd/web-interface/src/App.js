b97771d1a651d06cda7b724cf460aef39f0636ce
Wise-Wizard <saransh.shankar@gmail.com>
2023-08-08 01:18:05 +0530

    Added Sentiment Function

--- FrontEnd/web-interface/src/App.js
+++ FrontEnd/web-interface/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AboutUs,
   Chef,
@@ -9,24 +9,39 @@ import {
   Intro,
   Laurels,
   SpecialMenu,
+  ReviewInput,
+  ReviewOutput
 } from "./container";
 import { Navbar } from "./components";
 import "./App.css";
+import axios from "axios";
 
-const App = () => (
-  <div>
-    <Navbar />
-    <Header />
-    <AboutUs />
-    <SpecialMenu />
-    <Chef />
-    <Intro />
-    <Laurels />
-    <Gallery />
-    <FindUs />
-    <Footer />
-  </div>
-);
+const App = () => {
+  const [prediction, setPrediction] = useState(null);
+  const handlePredict = async (review) => {
+    let response = await axios.post("http://localhost:8000/predict/", {
+      customer_review: review,
+    });
+    let sentiment = response.data.sentiment_review;
+    setPrediction(sentiment);
+  };
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <ReviewInput onPredict={handlePredict} />
+      {prediction !== null && <ReviewOutput prediction={prediction} />}
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
 
--- FrontEnd/web-interface/src/container/index.js
+++ FrontEnd/web-interface/src/container/index.js
@@ -1,12 +1,14 @@
-import AboutUs from './AboutUs/AboutUs';
-import Chef from './Chef/Chef';
-import FindUs from './Findus/FindUs';
-import Footer from './Footer/Footer';
-import Gallery from './Gallery/Gallery';
-import Header from './Header/Header';
-import Intro from './Intro/Intro';
-import Laurels from './Laurels/Laurels';
-import SpecialMenu from './Menu/SpecialMenu';
+import AboutUs from "./AboutUs/AboutUs";
+import Chef from "./Chef/Chef";
+import FindUs from "./FindUs/FindUs";
+import Footer from "./Footer/Footer";
+import Gallery from "./Gallery/Gallery";
+import Header from "./Header/Header";
+import Intro from "./Intro/Intro";
+import Laurels from "./Laurels/Laurels";
+import SpecialMenu from "./Menu/SpecialMenu";
+import ReviewInput from "./Review/Review_Input";
+import ReviewOutput from "./Review/Review_Output";
 
 export {
   AboutUs,
@@ -18,4 +20,6 @@ export {
   Intro,
   Laurels,
   SpecialMenu,
+  ReviewInput,
+  ReviewOutput,
 };
