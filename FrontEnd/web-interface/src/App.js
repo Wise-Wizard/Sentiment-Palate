68727a1ce75e099724db10fcc8957b547666ff68
Wise-Wizard <saransh.shankar@gmail.com>
2023-08-07 12:20:21 +0530

    Deployed to Kubernetes

--- FrontEnd/web-interface/src/App.js
+++ FrontEnd/web-interface/src/App.js
@@ -1,24 +1,25 @@
-import React, { useState } from 'react';
-import ReviewInput from './Components/Review_Input';
-import ReviewOutput from './Components/Review_Output';
-import axios from "axios"
+import React, { useState } from "react";
+import ReviewInput from "./Components/Review_Input";
+import ReviewOutput from "./Components/Review_Output";
+import axios from "axios";
 
 function App() {
   const [prediction, setPrediction] = useState(null);
 
-  const handlePredict = (review) => {
-    // Perform API call to your ML model here
-    // Set the prediction using setPrediction(result)
-    // For simplicity, using a mock prediction
+  const handlePredict = async (review) => {
+    let response = await axios.post("http://localhost:8000/predict/", {
+      customer_review: review,
+    });
+    console.log(response)
     const mockPrediction = Math.random() < 0.5 ? 0 : 1;
     setPrediction(mockPrediction);
   };
 
   return (
     <div className="App">
-        <h1>Restaurant Review Sentiment Analysis</h1>
-        <ReviewInput onPredict={handlePredict} />
-        {prediction !== null && <ReviewOutput prediction={prediction} />}
+      <h1>Restaurant Review Sentiment Analysis</h1>
+      <ReviewInput onPredict={handlePredict} />
+      {prediction !== null && <ReviewOutput prediction={prediction} />}
     </div>
   );
 }
