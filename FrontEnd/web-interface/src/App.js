0f7574a624ec37e9d505f2df578f67eaea13ee19
Wise-Wizard <saransh.shankar@gmail.com>
2023-08-08 01:18:52 +0530

    Removed Comments

--- FrontEnd/web-interface/src/App.js
+++ FrontEnd/web-interface/src/App.js
@@ -10,7 +10,7 @@ import {
   Laurels,
   SpecialMenu,
   ReviewInput,
-  ReviewOutput
+  ReviewOutput,
 } from "./container";
 import { Navbar } from "./components";
 import "./App.css";
@@ -44,26 +44,3 @@ const App = () => {
 };
 
 export default App;
-
-// function App() {
-//   const [prediction, setPrediction] = useState(null);
-
-//   const handlePredict = async (review) => {
-//     let response = await axios.post("http://localhost:8000/predict/", {
-//       customer_review: review,
-//     });
-//     console.log(response.data)
-//     let sentiment = response.data.sentiment_review
-//     setPrediction(sentiment);
-//   };
-
-//   return (
-//     <div className="App">
-//       <h1>Restaurant Review Sentiment Analysis</h1>
-//       <ReviewInput onPredict={handlePredict} />
-//       {prediction !== null && <ReviewOutput prediction={prediction} />}
-//     </div>
-//   );
-// }
-
-// export default App;
